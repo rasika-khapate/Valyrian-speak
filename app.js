7fc7a7381201d7684830baa1c69c0aa4d6fa633e
Rasika <121392298+rasika-khapate@users.noreply.github.com>
2023-01-02 21:57:12 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-area");
+var textOutput = document.querySelector("#output");
+
+btnTranslate.addEventListener("click", clickEventHandler)
+
+var serverURL ="https://api.funtranslations.com/translate/valyrian.json"
+
+function getTranslationURL(text)
+{
+  return serverURL + "?" +"text=" + text
+}
+
+function errorHandler(error)
+{
+  console.log("error occured", error);
+  alert("something seems wrong with the server, please try again after some time");
+}
+
+function clickEventHandler() 
+{
+  
+var inputText = textInput.value;
+
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json =>
+    {var translatedText = json.contents.translated
+      textOutput.innerText = translatedText}
+    )
+  .catch(errorHandler)
+}
